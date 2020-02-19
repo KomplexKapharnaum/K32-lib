6a0f4e92c4eab7ba27987367b860952412440474
Pistache <clem.gangneux@gmail.com>
2020-02-19 16:09:55 +0100

    Add Button logic

--- src/K32_remote.cpp
+++ src/K32_remote.cpp
@@ -9,8 +9,16 @@
 
 /* Definition of GPÏO Ports */
 #if HW_REVISION == 1
+const uint8_t* BTN_PIN[NB_BTN] = {32, 33, 25, 26};
+
+
+  // const uint8_t BTN1_BLACK_PIN = 32;
+  // const uint8_t BTN2_PREV_PIN  = 33;
+  // const uint8_t BTN3_FWD_PIN   = 25;
+  // const uint8_t BTN4_GO_PIN    = 26;
 
 #elif HW_REVISION == 2
+const uint8_t BTN_PIN[NB_BTN] = {32, 33, 25, 26}; // Button1 = BlackOut ; Button2 = Previous ; Button3 = Forward ; Button4 = Go
 
 #else
   #error "HW_REVISION undefined or invalid. Should be 1 or 2"
@@ -21,19 +29,36 @@ K32_remote::K32_remote() {
   LOG("REMOTE: init");
 
   this->lock = xSemaphoreCreateMutex();
+  /* Init Buttons pins */
+  for (int i = 0 ; i < NB_BTN; i++)
+  {
+    this->buttons[i].pin = BTN_PIN[i];
+    this->buttons[i].state = LOW;
+  }
 
-  // Start task
+  // Start main task
   xTaskCreate( this->task,          // function
       "remote_task",       // task name
       1000,              // stack memory
       (void*)this,        // args
       3,                 // priority
       NULL);              // handler
+
+// Start read button state task
+xTaskCreate( this->read_btn_state,          // function
+    "read_btn_task",       // task name
+    1000,              // stack memory
+    (void*)this,        // args
+    3,                 // priority
+    NULL);              // handler
 };
 
 /*
  *   PRIVATE
  */
+
+
+
 
  void K32_remote::task(void * parameter) {
    K32_remote* that = (K32_remote*) parameter;
@@ -42,10 +67,72 @@ K32_remote::K32_remote() {
    while(true) {
      /* Main loop */
 
+     /* Check Prev and Fwd button */
+     if (  that->buttons[0].flag == 1)
+     {
+       that->macroIndex --;
+     } else if ( that->buttons[0].flag == 2) {
+
+       /* code */
+     }
+     if (  that->buttons[2].flag == 1)
+     {
+       that->macroIndex ++;
+     }
+
+
+
      /********/
 
 
      vTaskDelay( xFrequency );
 
+   }
+ }
+
+ void K32_remote::read_btn_state(void * parameter) {
+  K32_remote* that = (K32_remote*) parameter;
+   TickType_t xFrequency = pdMS_TO_TICKS(BTN_CHECK);
+   int lastButtonState = LOW ;
+   unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+   unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+   unsigned long longPushDelay = 1000; // Delay for a long push of the button
+
+
+   while (true)
+   {
+     for (int i = 0 ; i < NB_BTN ; i++)
+     {
+             // read the state of the switch into a local variable:
+       int reading = digitalRead(that->buttons[i].pin);
+
+       if (reading == HIGH)
+       {
+         if (that->buttons[i].state == LOW)
+         {
+           that->buttons[i].state = HIGH ;
+           that->buttons[i].lastPushTime = millis(); // Record time of pushing button
+         } else
+         {
+           if (that->buttons[i].lastPushTime>longPushDelay)
+           {
+             that->buttons[i].flag = 2; // Long push
+             that->buttons[i].lastPushTime = millis(); // Reset counter
+           }
+         }
+       } else
+       {
+         if (that->buttons[i].state == HIGH)
+         {
+           that->buttons[i].state = LOW ;
+           if (that->buttons[i].lastPushTime>debounceDelay)
+           {
+             that->buttons[i].flag = 1; // short push
+           }
+         }
+       }
+     }
+
+     vTaskDelay( xFrequency );
    }
  }
--- src/K32_remote.h
+++ src/K32_remote.h
@@ -7,12 +7,22 @@ Released under GPL v3.0
 #define K32_remote_h
 
 #define REMOTE_CHECK 100           // task loop in ms
+#define BTN_CHECK 10           // btn reading task loop in ms
+#define NB_BTN 4              // Number of push buttons
 
 class K32_remote;
+
 #include "K32_log.h"
 #include "K32.h"
 #include "Arduino.h"
 
+
+struct digitalbtn {
+  bool state; // State of button
+  uint8_t pin; // GPIO pin
+  int flag = 0 ; // 0 if nothing to do; 1 if a short press occured ; 2 if long press occured
+  unsigned long lastPushTime = 0;
+};
 
 class K32_remote {
   public:
@@ -21,9 +31,12 @@ class K32_remote {
 
   private:
     SemaphoreHandle_t lock;
-
+    digitalbtn buttons[NB_BTN] ;
+    int macroIndex = 0;
 
     static void task(void * parameter);
+    static void read_btn_state(void * parameter);
+
 };
 
 #endif
