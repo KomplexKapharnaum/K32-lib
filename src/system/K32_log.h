cee011b1eddbf0951007f96fb6fc19bf4a9c4165
richard fontaine <ririfonfon10@gmail.com>
2020-04-07 14:08:02 +0200

    add LOGHEX

--- src/system/K32_log.h
+++ src/system/K32_log.h
@@ -14,6 +14,7 @@
  #define LOGSETUP()                    Serial.begin(115200)
  #define LOGINL(x)                     Serial.print (x)
  #define LOGDEC(x)                     Serial.print (x, DEC)
+ #define LOGHEX(x)                     Serial.print (x, HEX)
  #define LOGF(x, y)                    Serial.printf (x, y)
  #define LOGF2(x, y1, y2)              Serial.printf (x, y1, y2)
  #define LOGF3(x, y1, y2, y3)          Serial.printf (x, y1, y2, y3)
