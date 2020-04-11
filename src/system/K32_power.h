c68f04d365f5c794eaaffcef35be54b08687361a
richard fontaine <ririfonfon10@gmail.com>
2020-04-11 11:41:04 +0200

    Update K32_power.h

--- src/system/K32_power.h
+++ src/system/K32_power.h
@@ -1,6 +1,6 @@
 /*
 K32_power.h
-Created by Clement GANGNEUX, june 2019.
+Created by Clement GANGNEUX, april 2020.
 Released under GPL v3.0
 */
 #ifndef K32_power_h
@@ -13,82 +13,75 @@ Released under GPL v3.0
 //#define SET_CURRENT_OFFSET 2890        // Set Current calibration offset ((necessary one time only))
 
 #define MAX_VOLTAGE_PROFILES 10
-#define POWER_CHECK 100           // task loop in ms
-#define CURRENT_SENSOR_TYPE 10    // Current sensor type : 0 = no sensor (info given by OSC) ;
-                                  // 10 = HO10-P/SP33
-                                  // 11 = H010
-                                  // 25 = HO25-P/SP33
+#define POWER_CHECK 100        // task loop in ms
+#define CURRENT_SENSOR_TYPE 10 // Current sensor type : 0 = no sensor (info given by OSC) ; \
+                               // 10 = HO10-P/SP33                                          \
+                               // 11 = H010                                                 \
+                               // 25 = HO25-P/SP33
 
-
-enum batteryType {
+enum batteryType
+{
   LIPO,
   LIFE,
 };
 
-struct voltageProfile {
-  unsigned int profile[7] ; 
-  unsigned int outputCurrent[2] ; // min and max value of outpur current 
-}; 
+struct voltageProfile
+{
+  unsigned int profile[7];
+  unsigned int outputCurrent[2]; // min and max value of outpur current
+};
 
 /* All voltages are given in mV */
-const unsigned int LIPO_VOLTAGE_BREAKS[] = {3500, 3650, 3700, 3750, 3825, 3950, 4200}; //For one cell 
+const unsigned int LIPO_VOLTAGE_BREAKS[] = {3500, 3650, 3700, 3750, 3825, 3950, 4200}; //For one cell
 const unsigned int LIFE_VOLTAGE_BREAKS[] = {2920, 3140, 3200, 3220, 3240, 3260, 3600}; //For one cell
-const unsigned int INITIAL_CELL_VOLTAGE_TOLERANCE = 50; // Tolerance added to the cell charged voltage
+const unsigned int INITIAL_CELL_VOLTAGE_TOLERANCE = 50;                                // Tolerance added to the cell charged voltage
 
+class K32_power
+{
+public:
+  K32_power(K32_stm32 *stm32, const int CURRENT_SENSOR_PIN);
 
-class K32_power {
-  public:
-    K32_power(K32_stm32* stm32, const int CURRENT_SENSOR_PIN);
+  void start();
+  void stop();
 
-    void start();
-    void stop();
+  int power();                                                            // Get instant Load power consumption (W)
+  int energy();                                                           // Get Energy consummed since last reset (Wh)
+  int current();                                                          // Get current from current sensor / STM32 if Current type = 0
+  void reset();                                                           // Reset energy time counter
+  void calibrate();                                                       // Calibrate current sensor. Call this function when Current flowing through sensor is 0.
+  void setAdaptiveGauge(bool adaptiveOn, batteryType type, int nbOfCell); // Function to activate adaptive gauge visualisation depending on current.
+                                                                          // Set adaptiveOn to true to set adaptive gauge algo
+                                                                          // type between LIPO and LIFE
+                                                                          // Set nbOfCell according to number of Cell ; 0 means auto determining number of Cells
+  void addVoltageProfile(unsigned int profile[7], unsigned int minOutputCurrent, unsigned int maxOutputCurrent);
 
-    int power();      // Get instant Load power consumption (W)
-    int energy();      // Get Energy consummed since last reset (Wh)
-    int current() ;    // Get current from current sensor / STM32 if Current type = 0
-    void reset();      // Reset energy time counter
-    void calibrate(); // Calibrate current sensor. Call this function when Current flowing through sensor is 0. 
-    void setAdaptiveGauge(bool adaptiveOn,batteryType type ,int nbOfCell); // Function to activate adaptive gauge visualisation depending on current. 
-                                                          // Set adaptiveOn to true to set adaptive gauge algo
-                                                          // type between LIPO and LIFE
-                                                          // Set nbOfCell according to number of Cell ; 0 means auto determining number of Cells
-    void addVoltageProfile (unsigned int profile[7] , unsigned int minOutputCurrent, unsigned int maxOutputCurrent); 
+  bool charge;
+  int SOC;
 
+private:
+  SemaphoreHandle_t lock;
+  K32_stm32 *_stm32;
+  int _power = 0;
+  int _energy = 0;
+  int _current = 0;
+  TaskHandle_t t_handle = NULL;
+  /* Current Sensor specs */
+  int currentPin;
+  int currentOffset = 1800; // Offset of current measurement
+  int currentFactor;        // Factor of current measurement
+  /* Adaptive Gauge variables */
+  bool adaptiveGaugeOn = false;
+  batteryType battType = LIPO;
+  uint8_t nbOfCell = 0;
+  voltageProfile profiles[MAX_VOLTAGE_PROFILES];
+  int profileIdx = 0; // Number of profile Idx available
+  int profileOn = -1; // Index of operating profile (-1 stands for default mode)
 
-    bool charge ;
-    int SOC ;
+  uint8_t findCellCount(unsigned int voltage, unsigned int cellMin, unsigned int cellMax);
+  void _lock();
+  void _unlock();
 
-
-  private:
-    SemaphoreHandle_t lock;
-    K32_stm32* _stm32;
-    int _power = 0;
-    int _energy = 0;
-    int _current = 0;
-    TaskHandle_t t_handle = NULL;
-    /* Current Sensor specs */ 
-    int currentPin ; 
-    int currentOffset = 1800 ;   // Offset of current measurement
-    int currentFactor;         // Factor of current measurement
-    /* Adaptive Gauge variables */ 
-    bool adaptiveGaugeOn = false; 
-    batteryType battType = LIPO; 
-    uint8_t  nbOfCell = 0; 
-    voltageProfile profiles[MAX_VOLTAGE_PROFILES]; 
-    int profileIdx = 0; // Number of profile Idx available
-    int profileOn = -1 ; // Index of operating profile (-1 stands for default mode)
-
-
-
-
-    uint8_t findCellCount(unsigned int voltage, unsigned int cellMin, unsigned int cellMax) ; 
-    void _lock();
-    void _unlock();
-
-
-    static void task(void * parameter);
-
-
+  static void task(void *parameter);
 };
 
 #endif
