cfec9292597665d1f4a93591f6f8fd46b7834734
richard fontaine <ririfonfon10@gmail.com>
2021-02-16 20:02:20 +0100

    Update K32_light.h

--- src/light/K32_light.h
+++ src/light/K32_light.h
@@ -6,16 +6,16 @@
 #ifndef K32_light_h
 #define K32_light_h
 
-#define LEDS_MAXSTRIPS    8    // There is 8 RMT channels on ESP32
-#define LEDS_ANIMATE_FPS  100   // Anims modulation FPS
-#define LEDS_SHOW_FPS     100   // Show RMT push FPS
-#define LEDS_ANIMS_SLOTS  16
-#define LEDS_MAX_COPY     16
-
+#define LEDS_MAXSTRIPS 8     // There is 8 RMT channels on ESP32
+#define LEDS_ANIMATE_FPS 100 // Anims modulation FPS
+#define LEDS_SHOW_FPS 100    // Show RMT push FPS
+#define LEDS_ANIMS_SLOTS 16
+#define LEDS_MAX_COPY 16
 
 #include "K32_ledstrip.h"
 #include "K32_anim.h"
 #include "system/K32_intercom.h"
+#include "k32_dmx.h"
 
 //
 // NOTE: to be able to use a modulator, it must be included here
@@ -39,62 +39,61 @@ struct stripcopy
   int destPos;
 };
 
+class K32_light
+{
+public:
+  K32_light();
 
+  void start();
 
-class K32_light {
-  public:
-    K32_light();
+  // STRIPS
+  //
+  void addStrip(const int pin, led_types type, int size = 0);
+  void cloneStrips(int masterStrip = 0);
+  void cloneDmxStrips(bool clone_Dmx , uint8_t clone_dmx_start);
+  void copyStrip(stripcopy copy);
 
-    void start();
-    
-    // STRIPS
-    //
-    void addStrip(const int pin, led_types type, int size = 0);
-    void cloneStrips(int masterStrip=0);
-    void copyStrip(stripcopy copy);
+  K32_dmx *setMultiple(int* values, int size, int offsetAdr);
 
-    K32_ledstrip* strip(int s);
-    K32_light* strips();
+  K32_ledstrip *strip(int s);
+  K32_light *strips();
 
-    K32_light* black();
-    K32_light* all(pixelColor_t color);
-    K32_light* all(int red, int green, int blue, int white = 0);
-    K32_light* pix(int pixel, pixelColor_t color);
-    K32_light* pix(int pixel, int red, int green, int blue, int white = 0);
+  K32_light *black();
+  K32_light *all(pixelColor_t color);
+  K32_light *all(int red, int green, int blue, int white = 0);
+  K32_light *pix(int pixel, pixelColor_t color);
+  K32_light *pix(int pixel, int red, int green, int blue, int white = 0);
 
+  void show();
+  void blackout();
 
-    void show();
-    void blackout();
+  //  ANIM
+  //
+  K32_anim *anim(String animName = "");
+  K32_anim *anim(int stripN, String animName, K32_anim *anim, int size = 0, int offset = 0);
+  void stop();
 
+  // Set FPS
+  void fps(int f = -1);
 
-    //  ANIM
-    //
-    K32_anim* anim( String animName = "");
-    K32_anim* anim( int stripN, String animName, K32_anim* anim, int size = 0, int offset = 0);
-    void stop();
+  void command(Orderz *order);
 
-    // Set FPS
-    void fps(int f = -1);
-    
-    void command(Orderz* order);
+private:
+  static int _nstrips;
+  K32_ledstrip *_strips[LEDS_MAXSTRIPS];
 
-  private:
-    
-    static int _nstrips;
-    K32_ledstrip* _strips[LEDS_MAXSTRIPS];
+  K32_anim *_anims[LEDS_ANIMS_SLOTS];
+  int _animcounter = 0;
 
-    K32_anim* _anims[LEDS_ANIMS_SLOTS];
-    int _animcounter = 0;
+  static void refresh(void *parameter);
+  int _fps = LEDS_SHOW_FPS;
 
-    static void refresh( void * parameter ) ;
-    int _fps = LEDS_SHOW_FPS;
+  int _masterClone = -1;
+  bool clone_Dmx;
+  uint8_t clone_dmx_start;
 
-    int _masterClone = -1;
-
-    int _copyMax = 0;
-    stripcopy _copylist[LEDS_MAX_COPY];
+  int _copyMax = 0;
+  stripcopy _copylist[LEDS_MAX_COPY];
 };
-
-
 
 #endif
