0e81f1366aab6a12f19131bccd438e4e867ee85b
richard fontaine <ririfonfon10@gmail.com>
2021-05-17 11:13:48 +0200

    pwm v1 good pin

--- K32-core/src/K32_version.h
+++ K32-core/src/K32_version.h
@@ -31,7 +31,7 @@ const int LEDS_PIN[MAX_HW+1][LED_N_STRIPS] = {
 #define PWM_N_CHAN 4
 const int PWM_PIN[MAX_HW+1][PWM_N_CHAN] = {
   {17, 16, -1, -1 },               // HW_REVISION 0
-  {17, 16, -1, -1 },               // HW_REVISION 1
+  {12, 13, -1, -1 },               // HW_REVISION 1
   {14, 12, 13, 15 },               // HW_REVISION 2
   {-1, -1, -1, -1 }                // HW_REVISION 3: ATOM
 };
