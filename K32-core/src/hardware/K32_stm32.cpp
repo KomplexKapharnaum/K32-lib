ebfe279f5f938ad146dc62b25e23b9262d9308e2
Clément SAILLANT <clement@lelectronrare.fr>
2023-01-27 15:57:50 +0100

    update API to HW v3.1

--- K32-core/src/hardware/K32_stm32.cpp
+++ K32-core/src/hardware/K32_stm32.cpp
@@ -78,6 +78,10 @@ int K32_stm32::firmware() {
   return this->get(K32_stm32_api::GET_FW_VERSION);
 };
 
+int K32_stm32::node_id() {
+  return this->get(K32_stm32_api::GET_BOARD_ID);
+};
+
 int K32_stm32::current() {
   return this->get(K32_stm32_api::GET_LOAD_CURRENT);
 };
