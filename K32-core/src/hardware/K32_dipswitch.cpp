e1ab034f6e9c5cf81b7eebb74d7117648b9de31e
richard fontaine <ririfonfon10@gmail.com>
2025-04-01 19:05:04 +0200

    range dip

--- K32-core/src/hardware/K32_dipswitch.cpp
+++ K32-core/src/hardware/K32_dipswitch.cpp
@@ -7,75 +7,76 @@
 #include "K32_version.h"
 #include "K32_dipswitch.h"
 
-// #include <ESPmDNS.h>
-
-
 /*
  *   PUBLIC
  */
 
-K32_dipswitch::K32_dipswitch(K32* k32) : K32_plugin("dips", k32) {
+K32_dipswitch::K32_dipswitch(K32 *k32) : K32_plugin("dips", k32)
+{
 
     this->lock = xSemaphoreCreateMutex();
 
-    for(int k=0; k<DIP_SLOTS; k++) this->watchPins[k] = 0;
-
-    xTaskCreate( this->watch,          // function
-                  "dips_watch",         // server name
-                  1500,              // stack memory
-                  (void*)this,        // args
-                  0,                  // priority
-                  NULL              // handler
-                  );                // core 
+    xTaskCreate(this->dipswatch, // function
+                "dips_watch",    // server name
+                1500,            // stack memory
+                (void *)this,    // args
+                0,               // priority
+                NULL             // handler
+    );                           // core
 }
 
-void K32_dipswitch::add(int pin, String name) 
-{   
-    for(int k=0; k<=DIP_SLOTS; k++) {
-        if (k==DIP_SLOTS) {LOG("dips: no more slots.."); return;}   // No more slots
-
-        if (this->watchPins[k]==0) 
-        {
-            pinMode(pin, INPUT_PULLUP);
-            this->watchValues[k] = HIGH;
-            this->watchNames[k]= name;
-            this->watchPins[k] = pin;
-            break;
-        }
-    }
-}
-
-void K32_dipswitch::add(int pin) {
+void K32_dipswitch::add(int pin)
+{
     this->add(pin, String(pin));
 }
-
-
-void K32_dipswitch::command(Orderz* order) {
-  // TODO: receive commands ?
-}
-
 
 /*
  *   PRIVATE
  */
 
-void K32_dipswitch::watch( void * parameter ) {
-    K32_dipswitch* that = (K32_dipswitch*) parameter;
+void K32_dipswitch::dipswatch(void *parameter)
+{
+    // K32_dipswitch *that = (K32_dipswitch *)parameter;
     TickType_t xFrequency = pdMS_TO_TICKS(10);
 
-    while(true) 
-    { 
-        for(int k=0; k<DIP_SLOTS; k++) 
-        {
-            if (that->watchPins[k]>0) {
-                int value = analogRead(that->watchPins[k]);
-                that -> emit( "dips/"+that->watchNames[k]+"value: "+value);
-            }
-        }
-
+    while (true)
+    {
+        // int value = analogRead(DIP_PIN[k32->system->hw()]);
+        // that->emit("dips/ value: " + value);
+        int value = analogRead(34);
+        if ((1650 < value) && (value < 1900))     LOG ("dip 1  "); 
+                      else if ( (2000<value) && (value<2250) )  LOG ("dip  2 ");
+                      else if ( (2280<value) && (value<2500) )  LOG ("dip   3");
+                      else if ( (2600<value) && (value<2820) )  LOG ("dip 12 ");
+                      else if ( (2850<value) && (value<2970) )  LOG ("dip 1 3");
+                      else if ( (2980<value) && (value<3100) )  LOG ("dip  23");
+                      else if ( (3200<value) && (value<3500) )  LOG ("dip 123"); 
       vTaskDelay( xFrequency );
     }
 
     vTaskDelete(NULL);
 }
 
+// while (true)
+// {
+//     k32->timer->every(10000, []()
+//                       {
+//         int value = analogRead(34);
+//         if ((1650 < value) && (value < 1900))     LOG ("dip 1  "); 
+// else if ( (2000<value) && (value<2250) )  LOG ("dip  2 ");
+// else if ( (2280<value) && (value<2500) )  LOG ("dip   3");
+// else if ( (2600<value) && (value<2820) )  LOG ("dip 12 ");
+// else if ( (2850<value) && (value<2970) )  LOG ("dip 1 3");
+// else if ( (2980<value) && (value<3100) )  LOG ("dip  23");
+// else if ( (3200<value) && (value<3500) )  LOG ("dip 123"); 
+// }
+
+// mesure de la pin
+// dip     = 0
+// dip 1   = 1770-1780
+// dip 2   = 2135-2142
+// dip 3   = 2380-2390
+// dip 12  = 2740-2750
+// dip 1 3 = 2950-2960
+// dip  23 = 2995-3005
+// dip 123 = 3369-3376
--- K32-core/src/hardware/K32_dipswitch.h
+++ K32-core/src/hardware/K32_dipswitch.h
@@ -10,7 +10,7 @@
 // #include "K32_wifi.h"
 #include <class/K32_plugin.h>
 
-#define DIP_SLOTS 8
+// #define DIP_SLOTS 8
 
 class K32_dipswitch : K32_plugin 
 {
@@ -20,15 +20,26 @@ class K32_dipswitch : K32_plugin
     void add(int pin, String name);
     void add(int pin);
 
-    void command(Orderz* order);
+    // void command(Orderz* order);
 
   private:
     SemaphoreHandle_t lock;
-    static void watch( void * parameter );
+    static void dipswatch( void * parameter );
 
-    int  watchPins[DIP_SLOTS];
-    String  watchNames[DIP_SLOTS];
-    bool watchValues[DIP_SLOTS];
+    // int  watchPins[DIP_SLOTS];
+    // String  watchNames[DIP_SLOTS];
+    // uint watchValues[DIP_SLOTS];
+    bool dip[3];
 };
 
 #endif
+
+// mesure de la pin
+// dip     = 0
+// dip 1   = 1770-1780
+// dip 2   = 2135-2142
+// dip 3   = 2380-2390
+// dip 12  = 2740-2750
+// dip 1 3 = 2950-2960
+// dip  23 = 2995-3005
+// dip 123 = 3369-3376
